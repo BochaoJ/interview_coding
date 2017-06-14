77f6d4fab52381f98a4ae6e04605d7d2b258566c
Bochao Jia <jbc409@gmail.com>
2017-06-14 16:19:24 -0400

    Create itinerary.py

--- python/itinerary.py
+++ python/itinerary.py
@@ -50,3 +50,35 @@ if __name__ == '__main__':
     for impl in implementations:
         print 'for %s in implementations' % impl
         print 'f(%s) == %s: %s' % (city_pairs, result, impl(city_pairs) == result)
+        
+        
+        
+  ## alter solution 
+
+ # City pairs format is (from, to)
+    city_pairs = [('SFO','LAX'),('BOS','DEN'),('LAX','BOS'),('JFK','SFO')]
+    result = [('JFK','SFO'),('SFO','LAX'),('LAX','BOS'),('BOS','DEN')]
+
+
+def city_sort(city_pairs):
+    first, second = [],[]
+    for city in city_pairs:
+           first.append(city[0])
+           second.append(city[1])
+    visited = [city_pairs[0]]
+
+    while len(visited)<len(city_pairs):
+      for index, dep in enumerate(second):
+        if dep == visited[0][0]:
+            visited.insert(0,city_pairs[index])
+
+        for index, dep in enumerate(first):
+            if dep == visited[-1][1]:
+                visited.append(city_pairs[index])
+
+    return visited
+
+city_sort(city_pairs)
+
+
+        
